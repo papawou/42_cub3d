d9d97610526961fc20cfadcc325031278ba3eb36
Kevin Mendes <kmendes@student.42lausanne.ch>
2022-10-22 21:13:46 +0200

    fix check_map function

--- src/parser/check_map.c
+++ src/parser/check_map.c
@@ -5,8 +5,8 @@ static _Bool	check_hole(int **map, t_vec2 len, t_vec2 pos)
 {
 	if ((pos.x == 0 || pos.x == len.x - 1 || pos.y == 0 || pos.y == len.y -1)) //check if empty case is not at map len border
 		return (false);
-	if (map[pos.y][pos.x - 1] != 1 || map[pos.y][pos.x + 1] != 1
-			|| map[pos.y - 1][pos.x] != 1 || map[pos.y + 1][pos.x] != 1) //check if empty case is adj to a wall
+	if (map[pos.y][pos.x - 1] == -1 || map[pos.y][pos.x + 1] == -1
+			|| map[pos.y - 1][pos.x] == -1 || map[pos.y + 1][pos.x] == -1) //check if empty case is adj to void (-1)
 		return (false);
 	return (true);
 }
@@ -18,6 +18,7 @@ _Bool check_map(int **map, t_vec2 len)
 	pos = (t_vec2) {0};
 	while (pos.y < len.y )
 	{
+		pos.x = 0;
 		while (pos.x < len.x)
 		{
 			if (map[pos.y][pos.x] == 0)
